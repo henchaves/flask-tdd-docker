9ae9b7ccddb85302461503e475e8b79a6a3ab360
Henrique Chaves <henriquechavesmm@gmail.com>
2021-06-18 05:35:36 -0300

    Create PostgreSQL dabatase

--- manage.py
+++ manage.py
@@ -2,9 +2,15 @@
 
 from flask.cli import FlaskGroup
 
-from src import app
+from src import app, db
 
 cli = FlaskGroup(app)
+
+@cli.command("recreate_db")
+def recreate_db():
+  db.drop_all()
+  db.create_all()
+  db.session.commit()
 
 if __name__ == "__main__":
   cli()
