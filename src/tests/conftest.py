5e33e5ca017cd45f9ebd5c48cabb7e9fcdbc6312
Henrique Chaves <henriquechavesmm@gmail.com>
2021-06-18 20:45:27 -0300

    Add tests (unit and functional)

---
+++ src/tests/conftest.py
@@ -0,0 +1,17 @@
+import pytest
+
+from src import app, db
+
+@pytest.fixture(scope="module")
+def test_app():
+  app.config.from_object("src.config.TestingConfig")
+  with app.app_context():
+    yield app
+
+@pytest.fixture(scope="module")
+def test_database():
+  db.create_all()
+  yield db
+  db.session.remove()
+  db.drop_all()
+  
