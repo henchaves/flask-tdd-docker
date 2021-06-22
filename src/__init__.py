b1e390db7365ddb219d92650089944a93f030438
Henrique Chaves <henriquechavesmm@gmail.com>
2021-06-22 17:28:24 -0300

    Create UsersList resource with POST method

--- src/__init__.py
+++ src/__init__.py
@@ -24,6 +24,9 @@ def create_app(script_info=None):
   from src.api.ping import ping_blueprint
   app.register_blueprint(ping_blueprint)
 
+  from src.api.users import users_blueprint
+  app.register_blueprint(users_blueprint)
+
   #shell context for flask cli
   @app.shell_context_processor
   def ctx():
---
+++ src/tests/test_users.py
@@ -0,0 +1,14 @@
+import json
+
+def test_add_user(test_app, test_database):
+  client = test_app.test_client()
+  resp = client.post("/users", data = json.dumps({
+                                      "username": "michael",
+                                      "email": "michael@testdriven.io"
+                                    }),
+                      content_type="application/json")
+  
+  data = json.loads(resp.data.decode())
+  
+  assert resp.status_code == 201
+  assert "michael@testdriven.io was added!" in data["message"]
