2eb580dcbbd1664abdbb7b8a9ef09b791ff68a6b
Henrique Chaves <henriquechavesmm@gmail.com>
2021-06-22 02:49:56 -0300

    Create Blueprint, adding Application Factory pattern.

--- src/__init__.py
+++ src/__init__.py
@@ -1,40 +1,32 @@
 #src/__init__.py
 
 import os
-from flask import Flask, jsonify
-from flask_restx import Resource, Api
+
+from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
-# instantiate the app
-app = Flask(__name__)
-api = Api(app)
-
-#set config
-app_settings = os.getenv("APP_SETTINGS")
-app.config.from_object(app_settings)
-
 #instantiate the db
-db = SQLAlchemy(app)
+db = SQLAlchemy()
 
-#model
-class User(db.Model):
-  __tablename__ = "users"
-  id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-  username = db.Column(db.String(128), nullable=False)
-  email = db.Column(db.String(128), nullable=False)
-  active = db.Column(db.Boolean(), default=True, nullable=False)
+def create_app(script_info=None):
+  
+  # instantiate the app
+  app = Flask(__name__)
 
-  def __init__(self, username, email):
-    self.username = username
-    self.email = email
+  #set config
+  app_settings = os.getenv("APP_SETTINGS")
+  app.config.from_object(app_settings)
 
+  #set up extensions
+  db.init_app(app)
 
-class Ping(Resource):
-  def get(self):
-    return {
-      "status": "success",
-      "message": "Pong!"
-    }
+  #register blueprints
+  from src.api.ping import ping_blueprint
+  app.register_blueprint(ping_blueprint)
 
-#Routes    
-api.add_resource(Ping, "/ping")
+  #shell context for flask cli
+  @app.shell_context_processor
+  def ctx():
+    return {"app": app, "db": db}
+  
+  return app
--- src/tests/conftest.py
+++ src/tests/conftest.py
@@ -1,17 +1,18 @@
 import pytest
 
-from src import app, db
+from src import create_app, db
 
 @pytest.fixture(scope="module")
 def test_app():
+  app = create_app()
   app.config.from_object("src.config.TestingConfig")
   with app.app_context():
-    yield app
+    yield app #testing happens here
 
 @pytest.fixture(scope="module")
 def test_database():
   db.create_all()
-  yield db
+  yield db #testing happends here
   db.session.remove()
   db.drop_all()
   
