0caff7650f58b72bdb7762a832cc73431da43446
Henrique Chaves M. de Menezes <henriquechavesmm@gmail.com>
2021-06-26 04:33:04 -0300

    Set up files to deploy to Heroku

--- src/config.py
+++ src/config.py
@@ -15,4 +15,8 @@ class TestingConfig(BaseConfig):
   SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_TEST_URL")
 
 class ProductionConfig(BaseConfig):
-  SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
+  url = os.environ.get("DATABASE_URL")
+  if url is not None and url.startswith("postgres://"):
+    url = url.replace("postgres://", "postgresql://", 1)
+
+  SQLALCHEMY_DATABASE_URI = url
