002ed753f09fc3e3fe087106867bafa9b6791d87
Henrique Chaves <henriquechavesmm@gmail.com>
2021-07-31 09:53:42 -0300

    Create Delete method and tests

--- src/api/users.py
+++ src/api/users.py
@@ -50,6 +50,19 @@ class Users(Resource):
             api.abort(404, f"User {user_id} does not exist")
         return user, 200
 
+    def delete(self, user_id):
+        response_object = {}
+        user = User.query.filter_by(id=user_id).first()
+
+        if not user:
+            api.abort(404, f"User {user_id} does not exist")
+
+        db.session.delete(user)
+        db.session.commit()
+
+        response_object["message"] = f"{user.email} was removed!"
+        return response_object, 200
+
 
 api.add_resource(UsersList, "/users")
 api.add_resource(Users, "/users/<int:user_id>")
