8d2bfd2e868ecb30a0160e74d31d5c48c81c9481
Henrique Chaves <henriquechavesmm@gmail.com>
2021-08-23 10:41:54 -0300

    Load api docs on production

--- src/__init__.py
+++ src/__init__.py
@@ -5,6 +5,7 @@ import os
 from flask import Flask
 from flask_admin import Admin
 from flask_sqlalchemy import SQLAlchemy
+from werkzeug.middleware.proxy_fix import ProxyFix
 
 # instantiate the db
 db = SQLAlchemy()
@@ -15,6 +16,7 @@ def create_app(script_info=None):
 
     # instantiate the app
     app = Flask(__name__)
+    app.wsgi_app = ProxyFix(app.wsgi_app, x_for=1, x_proto=1, x_host=1, x_port=1)
 
     # set config
     app_settings = os.getenv("APP_SETTINGS")
